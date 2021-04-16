411211732818b07cc6f725505752eacce7c66a74
Richard Klem <rklem@codasip.com>
2021-04-16 10:03:02 +0200

    Enhanced init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-mysql < init.sql
-mysql < data_tables.sql
-mysql < refreshers.sql
-mysql < refreshers_new.sql
-mysql < materialized_views.sql
+DB_NAME="xklemr00_IBT_test"
+# Change database name in init.sql
+mysql -e"set @db_name = '$DB_NAME'; \. init.sql"
+
+mysql -D $DB_NAME < data_tables.sql
+mysql -D $DB_NAME < refreshers.sql
+mysql -D $DB_NAME < refreshers_new.sql
+mysql -D $DB_NAME < materialized_views.sql
+
+# Import sample data
+mysql -D $DB_NAME < cl_environments.sql
+mysql -D $DB_NAME < cl_status.sql
+mysql -D $DB_NAME < artifacts.sql
+mysql -D $DB_NAME < sources.sql
+mysql -D $DB_NAME < artifacts_ip.sql
+mysql -D $DB_NAME < artifacts_session.sql
+mysql -D $DB_NAME < artifacts_studio.sql
+mysql -D $DB_NAME < tests_reduced.sql
