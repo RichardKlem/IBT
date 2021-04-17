05d62d6cb4d9ef37a56f9e5a93559933255c5e7e
Richard Klem <rklem@codasip.com>
2021-04-17 15:01:14 +0200

    Reformat, comments, rename.

--- init.sh
+++ init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-DB_NAME="xklemr00_IBT_test"
-# Change database name in init.sql
-mysql -e"set @db_name = '$DB_NAME'; \. init.sql"
 
+
+# Change DB name according to your preferences.
+DB_NAME="thousand_xklemr00_IBT"
+# ----------------------------------------------------------------------------------------------------------------------
+mysql -e"set @db_name = '$DB_NAME'; \. init.sql"
 mysql -D $DB_NAME < data_tables.sql
 mysql -D $DB_NAME < refreshers.sql
 mysql -D $DB_NAME < refreshers_new.sql
 mysql -D $DB_NAME < materialized_views.sql
-
 # Import sample data
 mysql -D $DB_NAME < cl_environments.sql
 mysql -D $DB_NAME < cl_status.sql
@@ -16,4 +17,7 @@ mysql -D $DB_NAME < sources.sql
 mysql -D $DB_NAME < artifacts_ip.sql
 mysql -D $DB_NAME < artifacts_session.sql
 mysql -D $DB_NAME < artifacts_studio.sql
-mysql -D $DB_NAME < tests_reduced.sql
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Choose one: tests_IDmod_100.sql is about 1 million rows, tests_IDmod_1000.sql is about 100k rows.
+mysql -D $DB_NAME < tests_IDmod_1000.sql
