527235cab74e737cdc904a0d61afadb2c20dbf85
Richard Klem <rklem@codasip.com>
2021-04-17 15:00:29 +0200

    Set max_allowed_packet to 16 MB

--- init.sql
+++ init.sql
@@ -9,4 +9,5 @@ prepare stmt from @stmt;
 execute stmt;
 deallocate prepare stmt;
 
+set GLOBAL max_allowed_packet = 1024*1024*16;
 # CREATE USER if not exists 'test_user'@'localhost' IDENTIFIED BY '12test5user';
