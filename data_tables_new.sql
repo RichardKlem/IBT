2c94aaef0bd968781c174d6db8337d18cd8a6500
Richard Klem <rklem@codasip.com>
2021-04-21 10:16:07 +0200

    Reformat

--- data_tables_new.sql
+++ data_tables_new.sql
@@ -1,46 +1,33 @@
 create table artifacts
 (
-    id      bigint auto_increment
-        primary key,
+    id      bigint auto_increment primary key,
     version varchar(10) null,
     build   varchar(45) null,
     created datetime    null,
-    type    varchar(10) null
+    type    varchar(10) null,
+    index ix_artifacts_build (build),
+    index ix_artifacts_created (created),
+    index ix_artifacts_type (type),
+    index ix_artifacts_version (version)
 );
 
-create index ix_artifacts_build
-    on artifacts (build);
-
-create index ix_artifacts_created
-    on artifacts (created);
-
-create index ix_artifacts_type
-    on artifacts (type);
-
-create index ix_artifacts_version
-    on artifacts (version);
 
 create table artifacts_ip
 (
-    id            bigint      not null
-        primary key,
+    id            bigint      not null primary key,
     name          varchar(50) null,
     configuration varchar(50) null,
     constraint artifacts_ip_ibfk_1
         foreign key (id) references artifacts (id)
-            on delete cascade
+            on delete cascade,
+    index ix_artifacts_ip_configuration (configuration),
+    index ix_artifacts_ip_name (name)
 );
 
-create index ix_artifacts_ip_configuration
-    on artifacts_ip (configuration);
-
-create index ix_artifacts_ip_name
-    on artifacts_ip (name);
 
 create table cl_environments
 (
-    id        varchar(10) not null
-        primary key,
+    id        varchar(10) not null primary key,
     os        varchar(20) null,
     arch      varchar(20) null,
     compiler  varchar(20) null,
@@ -49,16 +36,14 @@ create table cl_environments
 
 create table cl_status
 (
-    id          int auto_increment
-        primary key,
+    id          int auto_increment primary key,
     code        varchar(100) null,
     description varchar(500) null
 );
 
 create table artifacts_session
 (
-    id             bigint        not null
-        primary key,
+    id             bigint        not null primary key,
     command        varchar(1000) null,
     duration       int           null,
     node_name      varchar(100)  null,
@@ -75,19 +60,10 @@ create table artifacts_session
         foreign key (status_id) references cl_status (id)
 );
 
-create index ix_artifacts_session_environment_id
-    on artifacts_session (environment_id);
-
-create index ix_artifacts_session_passed
-    on artifacts_session (passed);
-
-create index ix_artifacts_session_status_id
-    on artifacts_session (status_id);
 
 create table artifacts_studio
 (
-    id             bigint      not null
-        primary key,
+    id             bigint      not null primary key,
     build_number   int         null,
     build_type     varchar(20) null,
     status_id      int         null,
@@ -98,42 +74,29 @@ create table artifacts_studio
     constraint artifacts_studio_ibfk_2
         foreign key (status_id) references cl_status (id),
     constraint artifacts_studio_ibfk_3
-        foreign key (environment_id) references cl_environments (id)
+        foreign key (environment_id) references cl_environments (id),
+    index ix_artifacts_studio_build_number (build_number),
+    index ix_artifacts_studio_build_type (build_type)
 );
-
-create index ix_artifacts_studio_build_number
-    on artifacts_studio (build_number);
-
-create index ix_artifacts_studio_build_type
-    on artifacts_studio (build_type);
-
-create index ix_artifacts_studio_environment_id
-    on artifacts_studio (environment_id);
-
-create index ix_artifacts_studio_status_id
-    on artifacts_studio (status_id);
 
 
 create table sources
 (
-    id          bigint auto_increment
-        primary key,
+    id          bigint auto_increment primary key,
     repository  varchar(100) null,
     commit      varchar(40)  null,
     branch      varchar(100) null,
     artifact_id bigint       null,
     constraint sources_ibfk_1
         foreign key (artifact_id) references artifacts (id)
-            on delete cascade
+            on delete cascade,
+    index ix_sources_branch (branch)
 );
 
-create index artifact_id
-    on sources (artifact_id);
 
 create table tests
 (
-    id          bigint auto_increment
-        primary key,
+    id          bigint auto_increment primary key,
     name        varchar(100)  null,
     tool        varchar(30)   null,
     kind        varchar(30)   null,
@@ -146,6 +109,7 @@ create table tests
     studio_id   bigint        null,
     session_id  bigint        null,
     status_id   int           null,
+    date        date          null,
     constraint tests_ibfk_1
         foreign key (ip_id) references artifacts (id),
     constraint tests_ibfk_2
@@ -156,11 +120,46 @@ create table tests
         foreign key (status_id) references cl_status (id),
 
     index ix_tests_design_path (design_path),
-    index ix_tests_ip_id (ip_id),
     index ix_tests_kind (kind),
     index ix_tests_name (name),
     index ix_tests_parameters (parameters),
     index ix_tests_tool (tool),
     index ix_tests_type (type),
-    index ix_tool_kind (tool, kind)
+    index ix_tests_tool_kind (tool, kind),
+    index ix_tests_date (date)
+);
+
+create table tests_compiler
+(
+    id          bigint auto_increment primary key,
+    name        varchar(100)  null,
+    tool        varchar(30)   null,
+    kind        varchar(30)   null,
+    passed      tinyint(1)    null,
+    parameters  varchar(250)  null,
+    design_path varchar(50)   null,
+    link        varchar(1000) null,
+    type        varchar(10)   null,
+    ip_id       bigint        null,
+    studio_id   bigint        null,
+    session_id  bigint        null,
+    status_id   int           null,
+    date        date          null,
+    constraint tests_ibfk_1
+        foreign key (ip_id) references artifacts (id),
+    constraint tests_ibfk_2
+        foreign key (studio_id) references artifacts (id),
+    constraint tests_ibfk_3
+        foreign key (session_id) references artifacts (id),
+    constraint tests_ibfk_4
+        foreign key (status_id) references cl_status (id),
+
+    index ix_tests_design_path (design_path),
+    index ix_tests_kind (kind),
+    index ix_tests_name (name),
+    index ix_tests_parameters (parameters),
+    index ix_tests_tool (tool),
+    index ix_tests_type (type),
+    index ix_tests_tool_kind (tool, kind),
+    index ix_tests_date (date)
 );
