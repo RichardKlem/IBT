e83882eb492217e621aebf1f31183e05eb3d2997
Richard Klem <rklem@codasip.com>
2021-05-09 01:39:18 +0200

    Update init script

--- init.sql
+++ init.sql
@@ -1,13 +1,25 @@
-set @stmt = concat('create database ', @db_name);
-prepare stmt from @stmt;
-execute stmt;
-deallocate prepare stmt;
+SET @stmt = CONCAT('CREATE DATABASE ', @db_name, '_old');
+PREPARE stmt FROM @stmt;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
+SET @stmt = CONCAT('CREATE DATABASE ', @db_name, '_new');
+PREPARE stmt FROM @stmt;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
 
-set @stmt = concat('grant all privileges on ', @db_name, '.* to \'rklem\'@\'localhost\';');
-prepare stmt from @stmt;
-execute stmt;
-deallocate prepare stmt;
+CREATE USER IF NOT EXISTS 'test_user'@'localhost' IDENTIFIED BY '12test5user';
 
-set GLOBAL max_allowed_packet = 1024*1024*16;
-# CREATE USER if not exists 'test_user'@'localhost' IDENTIFIED BY '12test5user';
+SET @stmt = concat('GRANT ALL PRIVILEGES ON ', @db_name, '_old.* TO \'test_user\'@\'localhost\';');
+PREPARE stmt FROM @stmt;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+SET @stmt = concat('GRANT ALL PRIVILEGES ON ', @db_name, '_new.* TO \'test_user\'@\'localhost\';');
+PREPARE stmt FROM @stmt;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+FLUSH PRIVILEGES;
+
+SET GLOBAL max_allowed_packet = 1024 * 1024 * 16;
